f47b0ae5f68265920ed33c95c1c99e1f44701c7c
Simone Ferrini <sferrini@hotmail.it>
2014-03-26 23:25:10 +0000

    Add takePicture and UI stuff

--- Simone/src/main/java/com/sferrini/simone/MainActivity.java
+++ Simone/src/main/java/com/sferrini/simone/MainActivity.java
@@ -1,5 +1,7 @@
 package com.sferrini.simone;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.PixelFormat;
 import android.graphics.drawable.Drawable;
 import android.hardware.Camera;
@@ -8,19 +10,25 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 //public class MainActivity extends ActionBarActivity implements SensorEventListener {
 public class MainActivity extends ActionBarActivity implements SurfaceHolder.Callback, SensorEventListener {
+
     //Camera
     private Camera camera;
     static private SurfaceView surfaceView;
@@ -30,10 +38,17 @@ public class MainActivity extends ActionBarActivity implements SurfaceHolder.Cal
 
     final String tag = "AccLogger";
 
+    //Sensor
     SensorManager sm = null;
+
+    Boolean isPerfect;
+
+    TextView message = null;
+
     TextView xAccView = null;
     TextView yAccView = null;
     TextView zAccView = null;
+
     FrameLayout fm = null;
 
     /** Called when the activity is first created. */
@@ -58,11 +73,23 @@ public class MainActivity extends ActionBarActivity implements SurfaceHolder.Cal
         xAccView = (TextView) findViewById(R.id.xbox);
         yAccView = (TextView) findViewById(R.id.ybox);
         zAccView = (TextView) findViewById(R.id.zbox);
+
+        message = (TextView) findViewById(R.id.message);
+
+        ImageView icona=(ImageView)findViewById(R.id.takeButton);
+        icona.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isPerfect) {
+                    camera.takePicture(null, null, after_photo);
+                }
+            }
+        });
     }
 
     //Camera
     public void surfaceCreated(SurfaceHolder holder) {
-        camera=Camera.open();
+        camera = Camera.open();
     }
     public void surfaceDestroyed(SurfaceHolder holder) {
         camera.release();
@@ -73,6 +100,34 @@ public class MainActivity extends ActionBarActivity implements SurfaceHolder.Cal
             camera.startPreview();}
         catch (IOException e){}
     }
+
+    Camera.PictureCallback after_photo = new Camera.PictureCallback() {
+
+        public void onPictureTaken(byte[] data, Camera camera) {
+
+            //salviamo la foto
+            String root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString();
+            File miaCartella = new File(root + "/SimonePhotos");
+            miaCartella.mkdirs();
+            String nomeFoto = "photo";
+            File file = new File(miaCartella,nomeFoto+".jpg");
+            if (file.exists ()) file.delete ();
+            try {
+                file.createNewFile();
+                FileOutputStream out = new FileOutputStream(file);
+                Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length, null);
+                bmp.compress(Bitmap.CompressFormat.JPEG, 90, out);
+                out.write(data,0,data.length);
+                out.flush();
+                out.close();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        	//la preview della camera si blocca quando scattiamo una foto, va fatta ripartire
+            camera.startPreview();
+        }
+    };
     //------
 
 
@@ -93,13 +148,18 @@ public class MainActivity extends ActionBarActivity implements SurfaceHolder.Cal
                 Drawable borderRed = getResources().getDrawable(R.drawable.border_red );
 
                 if (values[1] > -1 && values[1] < 1) {
+                    isPerfect = true;
+                    message.setText("");
                     fm.setBackground(borderGreen);
                 } else {
+                    isPerfect = false;
                     fm.setBackground(borderRed);
+                    message.setText("The phone is no straight");
                 }
             }
         }
     }
+
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
         Log.d(tag, "onAccuracyChanged: " + sensor + ", accuracy: " + accuracy);
