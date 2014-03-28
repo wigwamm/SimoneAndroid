10c8a771e3937733f8a515afd9fccdd113be4e37
Simone Ferrini <sferrini@hotmail.it>
2014-03-28 12:39:52 +0000

    Add preview; Add save-retake;

--- Simone/src/main/java/com/sferrini/simone/MainActivity.java
+++ Simone/src/main/java/com/sferrini/simone/MainActivity.java
@@ -11,7 +11,6 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.view.SurfaceHolder;
@@ -23,8 +22,6 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 //public class MainActivity extends ActionBarActivity implements SensorEventListener {
@@ -37,6 +34,13 @@ public class MainActivity extends ActionBarActivity implements SurfaceHolder.Cal
     ImageView flash;
     ImageView shutter;
     //-----
+
+    //Preview
+    ImageView preview;
+    ImageView retake;
+    ImageView save;
+    Bitmap bmp;
+    //------
 
     //SoundManager for shutter
     SoundManager soundManager;
@@ -73,6 +77,40 @@ public class MainActivity extends ActionBarActivity implements SurfaceHolder.Cal
 
         soundManager.init(MainActivity.this);
 
+        //Preview
+        preview = (ImageView)findViewById(R.id.preview);
+        retake = (ImageView)findViewById(R.id.retake);
+        save = (ImageView)findViewById(R.id.save);
+
+        retake.setVisibility(View.INVISIBLE);
+        save.setVisibility(View.INVISIBLE);
+
+        retake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                retake.setVisibility(View.INVISIBLE);
+                save.setVisibility(View.INVISIBLE);
+                preview.setImageBitmap(null);
+                shutter.setVisibility(View.VISIBLE);
+                message.setVisibility(View.VISIBLE);
+                bmp = null;
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 retake.setVisibility(View.INVISIBLE);
+                 save.setVisibility(View.INVISIBLE);
+                 preview.setImageBitmap(null);
+                 shutter.setVisibility(View.VISIBLE);
+                 message.setVisibility(View.VISIBLE);
+                 //Save in media
+                 MediaStore.Images.Media.insertImage(getContentResolver(), bmp, "photoSimone" , "SimonePhotos");
+                 bmp = null;
+             }
+        });
+
         shutter = (ImageView)findViewById(R.id.takeButton);
         shutter.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -80,6 +118,8 @@ public class MainActivity extends ActionBarActivity implements SurfaceHolder.Cal
                 if (isPerfect) {
                     flash = (ImageView)findViewById(R.id.flash);
                     flash.setBackgroundColor(Color.parseColor("#FFFFFFFF"));
+                    message.setVisibility(View.INVISIBLE);
+                    shutter.setVisibility(View.INVISIBLE);
                     camera.takePicture(null, null, after_photo);
                     soundManager.play();
                 }
@@ -107,26 +147,18 @@ public class MainActivity extends ActionBarActivity implements SurfaceHolder.Cal
 
             flash.setBackgroundColor(Color.parseColor("#00000000"));
 
-            //salviamo la foto
-            String root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString();
-            File miaCartella = new File(root + "/SimonePhotos");
-            miaCartella.mkdirs();
-
-            String nomeFoto = "lastPhoto";
-            File file = new File(miaCartella,nomeFoto+".jpg");
-            if (file.exists ()) file.delete ();
+            //Save picture
             try {
-                file.createNewFile();
-                FileOutputStream out = new FileOutputStream(file);
-                Bitmap bmp = BitmapFactory.decodeByteArray(data, 0, data.length, null);
-                bmp.compress(Bitmap.CompressFormat.JPEG, 90, out);
+                bmp = BitmapFactory.decodeByteArray(data, 0, data.length, null);
+                //bmp.compress(Bitmap.CompressFormat.JPEG, 90, out);
 
                 //Save in media
-                MediaStore.Images.Media.insertImage(getContentResolver(), bmp, "photoSimone" , "SimonePhotos");
+                //MediaStore.Images.Media.insertImage(getContentResolver(), bmp, "photoSimone" , "SimonePhotos");
 
-                out.write(data,0,data.length);
-                out.flush();
-                out.close();
+                preview.setImageBitmap(bmp);
+
+                retake.setVisibility(View.VISIBLE);
+                save.setVisibility(View.VISIBLE);
 
             } catch (Exception e) {
                 e.printStackTrace();
